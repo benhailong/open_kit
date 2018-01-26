3c67568e044d1fb3feab169ebc252ca121ab2aec
benhailong <694201656@qq.com>
2018-01-26 17:06:08 +0800

    增加了聚合数据中的充值话费

---
+++ src/main/java/cc/openkit/kitJuhe/HuaFei/config/HuafeiConfig.java
@@ -0,0 +1,87 @@
+package cc.openkit.kitJuhe.HuaFei.config;
+
+public class HuafeiConfig {
+    private  String key;//申请的接口Appkey
+    private  String openId;//在个人中心查询
+    private  String telCheckUrl = "http://op.juhe.cn/ofpay/mobile/telcheck?cardnum=*&phoneno=!&key="+key;
+    private  String telQueryUrl="http://op.juhe.cn/ofpay/mobile/telquery?cardnum=*&phoneno=!&key="+key;
+    private  String onlineUrl="http://op.juhe.cn/ofpay/mobile/onlineorder?key="+key+"&phoneno=!&cardnum=*&orderid=@&sign=$";
+    private  String yueUrl="http://op.juhe.cn/ofpay/mobile/yue?key="+key+"&"+"timestamp=%&sign=$";
+    private  String orderstaUrl="http://op.juhe.cn/ofpay/mobile/ordersta?key="+key+"&orderid=!";
+    private  String orderListUrl="http://op.juhe.cn/ofpay/mobile/orderlist?key="+key;
+
+    public HuafeiConfig(){
+
+    }
+
+    public HuafeiConfig(String key, String openId){
+        this.key = key;
+        this.openId = openId;
+
+    }
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getTelCheckUrl() {
+        return telCheckUrl;
+    }
+
+    public void setTelCheckUrl(String telCheckUrl) {
+        this.telCheckUrl = telCheckUrl;
+    }
+
+    public String getTelQueryUrl() {
+        return telQueryUrl;
+    }
+
+    public void setTelQueryUrl(String telQueryUrl) {
+        this.telQueryUrl = telQueryUrl;
+    }
+
+    public String getOnlineUrl() {
+        return onlineUrl;
+    }
+
+    public void setOnlineUrl(String onlineUrl) {
+        this.onlineUrl = onlineUrl;
+    }
+
+    public String getYueUrl() {
+        return yueUrl;
+    }
+
+    public void setYueUrl(String yueUrl) {
+        this.yueUrl = yueUrl;
+    }
+
+    public String getOrderstaUrl() {
+        return orderstaUrl;
+    }
+
+    public void setOrderstaUrl(String orderstaUrl) {
+        this.orderstaUrl = orderstaUrl;
+    }
+
+    public String getOrderListUrl() {
+        return orderListUrl;
+    }
+
+    public void setOrderListUrl(String orderListUrl) {
+        this.orderListUrl = orderListUrl;
+    }
+}
